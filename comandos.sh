c05028621406aad049719619044c89e5e7b05434
PedroHenriqueMartinez <53578076+PedroHenriqueMartinez@users.noreply.github.com>
2019-08-06 17:11:34 -0300

    Add files via upload

---
+++ comandos.sh
@@ -0,0 +1,63 @@
+    1  ls
+    2  ls-lh
+    3  ls -lh
+    4  cd /
+    5  ls
+    6  cd -
+    7  pwd
+    8  mkdir linuxmint
+    9  ls
+   10  cd linuxmint/
+   11  ls
+   12  sudo apt update &&
+   13  sudo apt update && sudo apt install git
+   14  git clone https://github.com/PedroHenriqueMartinez/linuxMint.git
+   15  ls
+   16  ls -lh
+   17  cd linuxmint
+   18  ls -lh
+   19  cd -
+   20  ls
+   21  cd linuxmint/
+   22  ls -lh
+   23  ls
+   24  git clone https://github.com/PedroHenriqueMartinez/linuxMint.git
+   25  ls
+   26  cd linuxMint/
+   27  ls -lh
+   28  echo SENAC Tatuapé aprendendo Bash Shell no Linux Mint > pedro.sh
+   29  ls
+   30  ls -lh
+   31  touch senac.sh
+   32  ls -lh
+   33  cat LICENSE
+   34  cat -n LICENSE
+   35  head LICENSE
+   36  head -n20 LICENSE
+   37  tail LICENSE
+   38  tail -n20 LICENSE
+   39  less LICENSE
+   40  cat -n LICENSE | tail
+   41  cat -n100 LICENSE
+   42  cat -n LICENSE | tail 100
+   43  cat -n100 LICENSE | tail -n100
+   44  cat -n LICENSE | tail -n100
+   45  cat -n100 LICENSE
+   46  cat -n100 LICENSE | tail -n200
+   47  head -n20 LICENSE
+   48  cat -n100 LICENSE | head -n200
+   49  cat -n100 LICENSE | head -n200 | tail
+   50  cat -n100 LICENSE | head -n200 
+   51  cat -n100 LICENSE | head -n200 | tail 20 
+   52  cat -n100 LICENSE | head -n200 | tail 200
+   53  cat -n100 LICENSE | head -n200 | tail -n200
+   54  cat -n100 LICENSE | head -n200 | tail -n100
+   55  cat -n100 LICENSE | head -n200 | tail 100
+   56  cat -n LICENSE | head -n200 | tail 100
+   57  cat -n LICENSE | head 200 | tail -n100
+   58  cat -n LICENSE | head -n200 | tail -n100
+   59  cat -n LICENSE | head -n200 | tail -n101
+   60  cat -n LICENSE | head -n200 | tail -n101 less
+   61  cat -n LICENSE | head -n200 | tail -n101 | less
+   62  history
+   63  history > comandos.sh
